8ce6aa788573aa10758375d58881f03ff438db16
Eric Scrivner <eric@grapheffect.com>
2013-03-15 22:21:22 -0700

    Add __repr__ To BaseVertex and BaseEdge

--- machete/base.py
+++ machete/base.py
@@ -8,8 +8,14 @@ setup(["localhost"], "machete")
 class BaseVertex(thunderdome.Vertex):
     created_at = thunderdome.DateTime(default=datetime.now)
 
+    def __repr__(self):
+        return "<{}:{}>".format(self.__class__.__name__, self.vid)
+
 class BaseEdge(thunderdome.Edge):
     created_at = thunderdome.DateTime(default=datetime.now)
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.__class__.__name__, self.eid)
 
 
 class CreatedBy(BaseEdge):
