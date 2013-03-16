73457f6c4a0dde5b5eb2c35992cd0f4d221cea06
Eric Scrivner <eric@grapheffect.com>
2013-03-15 22:21:39 -0700

    Add Properties and Caliber Relationship Between Issue and Severity

--- machete/issues/models.py
+++ machete/issues/models.py
@@ -1,13 +1,64 @@
-
 import thunderdome
 from machete.base import BaseVertex, BaseEdge
 
+
 class Issue(BaseVertex):
+    """Represents an issue in machete and associated information."""
     description = thunderdome.String()
 
+    @property
+    def severity(self):
+        """
+        Returns the severity associated with this issue
+
+        :rtype: machete.issues.models.Severity
+        
+        """
+        result = self.outV(Caliber)
+        # Ensure this invariant holds as each issue should have one severity
+        assert len(result) <= 1
+        return result[0]
+
+        
 class Severity(BaseVertex):
+    """Indicates the severity of an issue"""
     name = thunderdome.String()
 
-class AssignedTo(BaseEdge):
-    pass
+    @property
+    def issues(self):
+        """
+        Return a list of issues associated with this severity.
 
+        :rtype: list
+        
+        """
+        return self.inV(Caliber)
+
+        
+class Caliber(BaseEdge):
+    """Edge connecting an issue to its severity"""
+
+    @property
+    def issue(self):
+        """
+        Return the issue associated with this caliber.
+
+        :rtype: machete.issues.models.Issue
+                
+        """
+        return self.outV()
+
+    @property
+    def severity(self):
+        """
+        Return the severity associated with this caliber.
+
+        :rtype: machete.issues.models.Severity
+        
+        """
+        return self.inV()
+        
+
+class AssignedTo(BaseEdge):
+    """Edge associating an issue with a particular user or users"""
+    pass
