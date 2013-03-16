3db1ba530ea21baac3f42f56a2a49e85d8d9d18f
Eric Scrivner <eric@grapheffect.com>
2013-03-15 22:21:54 -0700

    Add Unit-Tests Around Issue and Severity Creation and Association

--- machete/issues/tests/test_create.py
+++ machete/issues/tests/test_create.py
@@ -1,8 +1,21 @@
-
-from machete.issues.models import Issue
+from machete.issues.models import Issue, Severity, Caliber, AssignedTo
 
 import unittest
+
+
 class CreateTest(unittest.TestCase):
-    pass
+    """Unit-tests around the creation of issues"""
 
+    def setUp(self):
+        super(CreateTest, self).setUp()
+    
+    def test_should_be_able_to_create_new_issue(self):
+        """Should be able to create a new issue and get all related objects"""
+        issue = Issue.create(description="Hey Jon, here's a bug for ya!")
+        severity = Severity.create(name="Low Unbreak Now!")
+        caliber = Caliber.create(issue, severity)
 
+        assert issue.severity == severity
+        assert issue in severity.issues
+        assert caliber.issue == issue
+        assert caliber.severity == severity
