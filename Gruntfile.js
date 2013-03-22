f152f4d9f0d8341acb5d224137dc44fe96e0ddd5
Jon Haddad <jon@jonhaddad.com>
2013-03-22 09:36:05 -0700

    coffeescript is compiling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,25 @@
 
-grunt.loadNpmTasks('grunt-contrib-coffee');
+module.exports = function(grunt) {
 
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
+        coffee: {
+            compile: {
+                files: {
+                        'static/js/machete.js': ['static/coffeescript/*.coffee'] // compile and concat into single file
+                }
+            }
+        }
+
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+
+    // Default task(s).
+    //grunt.registerTask('default', ['uglify']);
+
+};
---
+++ static/coffeescript/machete.coffee
@@ -0,0 +1,2 @@
+
+machete = true
